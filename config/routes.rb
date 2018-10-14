5743091bb5df31c85b2b432de89032d72ae1e109
Everett <everett.bowline@gmail.com>
2018-10-14 22:22:13 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-    get '/login' => 'sessions#new'
+  get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
 
