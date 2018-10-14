d722aef488266dfe01fd058366088694194e1393
Everett <everett.bowline@gmail.com>
2018-10-14 22:28:50 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to 'welcome/home'
     else
-      redirect to root_path
+      redirect_to root_path
     end
   end
 
