b31e2a3da606dc948d8b1b0582ae88459a4496af
Everett <everett.bowline@gmail.com>
2018-10-14 21:57:00 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,4 +15,9 @@ class UsersController < ApplicationController
     end
   end
 
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :password, :password_confirmation)
+
 end
