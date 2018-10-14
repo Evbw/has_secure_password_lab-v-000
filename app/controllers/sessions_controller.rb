d177603dd56cded183b041cbc106bf7a0d4727c6
Everett <everett.bowline@gmail.com>
2018-10-14 22:08:11 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,11 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(name: params[:user][:name])
 
-    if @user && @user.authenticate(params[:user][:password])
+    if @user && @user.try(:authenticate, params[:user][:password])
       session[:user_id] = @user.id
     else
       redirect to login_path
     end
   end
+
 end
